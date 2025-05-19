15400cee34bd35501cd13e2e8f4f9442f66a19f6
railsnjoyer <railsenjoyer@gmail.com>
2025-05-19 16:20:46 +0200

    Added variations for 1 task

--- subquery&CTE.sql
+++ subquery&CTE.sql
@@ -6,3 +6,28 @@ FROM (
   GROUP BY user_id
 ) AS sub
 JOIN users ON users.id = sub.user_id;
+
+-- CTE ver. 
+
+WITH recent_orders AS (
+  SELECT * FROM orders WHERE created_at >= DATE('now', '-1 month')
+),
+max_orders AS (
+  SELECT user_id, MAX(price) AS max_price
+  FROM recent_orders
+  GROUP BY user_id
+)
+SELECT users.name, max_orders.max_price
+FROM max_orders
+JOIN users ON users.id = max_orders.user_id;
+
+-- AR ver.
+
+subquery = Order
+  .where('created_at >= ?', 1.month.ago)
+  .select('user_id, MAX(price) as max_price')
+  .group('user_id')
+
+User
+  .joins("JOIN (#{subquery.to_sql}) AS sub ON sub.user_id = users.id")
+  .select('users.name, sub.max_price')
