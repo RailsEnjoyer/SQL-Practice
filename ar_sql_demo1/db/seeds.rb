5affaf47055bf2d39d49284df54e1c7fa1b29e5d
railsnjoyer <railsenjoyer@gmail.com>
2025-05-19 16:12:06 +0200

    initial commit

--- ar_sql_demo1/db/seeds.rb
+++ ar_sql_demo1/db/seeds.rb
@@ -2,11 +2,15 @@ u1 = User.create!(name: 'Иван')
 u2 = User.create!(name: 'Ольга')
 u3 = User.create!(name: 'Пётр')
 
-# Создадим товары
 p1 = Product.create!(name: 'Ruby Book', price: 100)
 p2 = Product.create!(name: 'Rails Mug', price: 20)
-p3 = Product.create!(name: 'DB Cheat Sheet', price: 5)
+p3 = Product.create!(name: 'DB Cheat Sheet', price: 5) #added to catch NULL when needed
 
 Order.create!(user: u1, product: p1, quantity: 1)
 Order.create!(user: u1, product: p1, quantity: 2)
+Order.create!(user: u1, product: p2, quantity: 2)
 Order.create!(user: u2, product: p2, quantity: 3)
+
+# ivan - 3 orders - 5 products
+# olga - 1 order - 3 products
+# petr - no orders
